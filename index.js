9c81d2e3d461c9d7fb070d000e40d67994450075
Khaled Osman <khaled@colorfy.me>
2018-10-10 17:55:17 +0200

    get folders with package.json return their name and path

--- index.js
+++ index.js
@@ -3,6 +3,16 @@
 const program = require('commander')
 const figlet = require('figlet')
 const { getCLIVersion } = require('./helpers/get-cli-version')
+const fs = require('fs')
+const { promisify } = require('util')
+const lstat = promisify(fs.lstat)
+const readDir = promisify(fs.readdir)
+const readFile = promisify(fs.readFile)
+const path = require('path')
+const chalk = require('chalk')
+const { checkFileExists } = require('./helpers/check-file-exists')
+
+const results = []
 
 initCli()
 
@@ -13,12 +23,13 @@ async function initCli () {
     .version(version, '-v, --version')
 
   program
-    .option('-f, --force', 'skip confirmation and overwrite existing zipfile')
-    .option('-s, --skipBuild', 'specifies whether to trigger webpack build or not')
-    .option('-o, --outputPath <path>', 'the path where the zipfile will be written, defaults to current working directory the user is running the cli from')
-    .description('creates zipfile for a plugin')
-    .action((path = './', options) => {
-      console.log('building plugin in %s', path)
+    .option('-p, --path', 'path to root directory to start scanning for imports')
+    .description('Analyze directory and print aliases for each directory that can be used as imports for react-native')
+    .action(async (path = process.cwd(), options) => {
+      analyzeDirectory(path)
+        .then(() => {
+          console.log('results', results)
+        })
     })
 
   console.log(figlet.textSync('Aliased Imports', {
@@ -32,4 +43,51 @@ async function initCli () {
   if (!process.argv.slice(2).length) {
     program.help()
   }
+}
+
+function analyzeDirectory (dirPath) {
+  return checkPackageJson(dirPath)
+    .then(async hasPackageJson => {
+      if (hasPackageJson) {
+        console.log(dirPath, 'hasPackageJson')
+        const name = getNameFromPackageJson(dirPath)
+        results.push({ name, dirPath })
+      }
+      return readDir(dirPath)
+    })
+    .then(subFiles => getSubfilesThatAreDirectories(dirPath, subFiles))
+    .then(subDirectories => {
+      console.log('dirPath', dirPath, 'subDirectories', subDirectories)
+      return Promise.all(subDirectories.map(subDirectory => analyzeDirectory(path.join(dirPath, subDirectory))))
+    })
+}
+
+async function getNameFromPackageJson (dirPath) {
+  const fileContent = await readFile(path.join(dirPath, 'package.json'), 'utf8')
+  const name = JSON.parse(fileContent).name
+  return name
+}
+
+function checkPackageJson (dirPath) {
+  return lstat(dirPath)
+    .then(fileStats => {
+      const isDirectory = fileStats.isDirectory()
+      if (isDirectory) {
+        return checkFileExists(path.join(dirPath, 'package.json'))
+      } else {
+        throw Error(`not a directory ${dirPath}`)
+      }
+    })
+}
+
+function getSubfilesThatAreDirectories (dirPath, subFiles) {
+  return Promise.all(subFiles.map(async subFile => {
+    const stats = await lstat(path.join(dirPath, subFile))
+    return {
+      isDirectory: stats.isDirectory(),
+      subFile
+    }
+  }))
+    .then(items => items.filter(item => item.isDirectory && !item.subFile.endsWith('node_modules') && !item.subFile.endsWith('.git')))
+    .then(items => items.map(i => i.subFile))
 }
